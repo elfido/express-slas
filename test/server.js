eddc00108fafc612ce3615c63412d370fecff1f9
Fido <fidencio.garrido@gmail.com>
2016-07-23 20:28:36 +0000

    Adding test cases

---
+++ test/server.js
@@ -0,0 +1,30 @@
+/**
+ * Used to test default behavior
+ */
+'use strict';
+
+var express = require("express"),
+    slas = require("..");
+ 
+var createServer = function(port, options) {
+    let app = express(),
+        opts = (typeof options === "object") ? options : {};
+    
+    app.use(slas(opts));
+ 
+    app.get("/success", function(req, res) {
+        res.send("cool");
+    });
+    
+    app.get("/slow", function(req, res){
+       setTimeout(function(){
+           res.send("not good");
+       }, 100); 
+    });
+ 
+    return app.listen(port, function(){
+        console.log("server is running");
+    });
+};
+ 
+module.exports = createServer;
---
+++ test/slas.spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var server = require("./server"),
+    request = require("supertest"),
+    assert = require("chai").assert,
+    port = 3001,
+    app;
+
+describe("Using default server with default settings", function(){
+    before(function(){
+        app = server( port );    
+    });
+    
+    after(function(){
+       app.close(); 
+    });
+    
+    describe("Starting server", function(){
+        it("Should succeed", function(done){
+            request(app).get("/success").expect(200);
+            done();
+        });
+    });
+});
+
+describe("Using default server with callback settings", function(done){
+    before(function(){
+        let settings = {
+            sla: 1000,
+            cb: function(time){
+                assert.equal(true,false, `Callback should not be executed in a normal call, executed after ${time} ms`);
+                done();
+            }
+        };
+        app = server( port, settings );    
+    });
+    
+    after(function(){
+      app.close(); 
+    });
+    
+    describe("Starting server", function(){
+        it("Should not apply callback", function(done){
+            request(app).get("/success").expect(200).end(function(req, res){
+                done();
+            });
+        });
+    });
+});
+
+
+describe("Using default server with callback settings, callback should be executed", function(complete){
+    let executed = false;
+    
+    before(function(){
+        let settings = {
+            sla: 90,
+            logError: false,
+            cb: function(time){
+                console.log("Callback executed as expected");
+                executed = true;
+            }
+        };
+        app = server( port, settings );    
+    });
+    
+    after(function(){
+      app.close(); 
+    });
+    
+    describe("Starting server", function(){
+        it("Should not apply callback", function(done){
+            request(app).get("/slow").expect(200).end(function(req, res){
+                setTimeout(function(){
+                    assert.equal(executed, true, "Callback is expected to be executed");
+                }, 10);
+                done();
+            });
+        });
+    });
+});
